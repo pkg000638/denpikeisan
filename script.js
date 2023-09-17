f59e924dbf3612746c52abe4b0c527fce8f4777c
pkg000638 <127655631+pkg000638@users.noreply.github.com>
2023-09-17 14:23:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function calculateCost() {
+    const recommendedCar = document.getElementById("recommendedCar").value;
+    const multiplicationFactor = parseFloat(document.getElementById("multiplicationFactor").value);
     const mileage = parseFloat(document.getElementById("mileage").value);
     const gasolineCost = parseFloat(document.getElementById("gasolineCost").value);
     const electricityCost = parseFloat(document.getElementById("electricityCost").value);
     const monthlyDistance = parseFloat(document.getElementById("monthlyDistance").value);
 
-    const gasolineCostPerKm = gasolineCost / mileage;
-    const electricityCostPerKm = electricityCost / 10; // 1kWの電気代を1kmあたりに変換
+    let gasolineCostPerKm;
+    let electricityCostPerKm;
+    let variableValue;
+
+    if (recommendedCar === "leaf") {
+        variableValue = 8.05;
+    } else if (recommendedCar === "aria") {
+        variableValue = 7.12;
+    } else if (recommendedCar === "sakura") {
+        variableValue = 9;
+    }
+
+    gasolineCostPerKm = gasolineCost / mileage;
+    electricityCostPerKm = gasolineCost / (variableValue * multiplicationFactor);
     const fuelCostDifference = (electricityCostPerKm - gasolineCostPerKm) * monthlyDistance;
 
     document.getElementById("gasolineCostPerKm").textContent = gasolineCostPerKm.toFixed(2);
-    document.getElementById("electricityCostPerKm").textContent = electricityCostPerKm.toFixed(2); // 1kmあたりの電気代を表示
+    document.getElementById("electricityCostPerKm").textContent = electricityCostPerKm.toFixed(2);
     document.getElementById("fuelCostDifference").textContent = fuelCostDifference.toFixed(2);
 }
