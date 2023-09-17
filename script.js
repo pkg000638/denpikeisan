b214c1f43a4177d7bd1137a6f10a6b3a01a1c03d
pkg000638 <127655631+pkg000638@users.noreply.github.com>
2023-09-17 10:54:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function calculateCost() {
     const monthlyDistance = parseFloat(document.getElementById("monthlyDistance").value);
 
     const gasolineCostPerKm = gasolineCost / mileage;
-    const electricityCostPerKm = electricityCost / 10; // 1kW = 10km分
+    const electricityCostPerKm = electricityCost / 10; // 1kWの電気代を1kmあたりに変換
     const fuelCostDifference = (electricityCostPerKm - gasolineCostPerKm) * monthlyDistance;
 
     document.getElementById("gasolineCostPerKm").textContent = gasolineCostPerKm.toFixed(2);
+    document.getElementById("electricityCostPerKm").textContent = electricityCostPerKm.toFixed(2); // 1kmあたりの電気代を表示
     document.getElementById("fuelCostDifference").textContent = fuelCostDifference.toFixed(2);
 }
