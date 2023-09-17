e4ba48e575e144ea29b6795bee02dce5defc03b2
pkg000638 <127655631+pkg000638@users.noreply.github.com>
2023-09-17 14:57:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculateCost() {
     const recommendedCar = document.getElementById("recommendedCar").value;
-    const multiplicationFactor = parseFloat(document.getElementById("multiplicationFactor").value);
+    const catalogNotation = parseFloat(document.getElementById("catalogNotation").value); // カタログ表記の選択肢
     const mileage = parseFloat(document.getElementById("mileage").value);
     const gasolineCost = parseFloat(document.getElementById("gasolineCost").value);
     const electricityCost = parseFloat(document.getElementById("electricityCost").value);
@@ -19,7 +19,7 @@ function calculateCost() {
     }
 
     gasolineCostPerKm = gasolineCost / mileage;
-    electricityCostPerKm =  electricityCost / (variableValue * multiplicationFactor);
+    electricityCostPerKm = electricityCost / (variableValue * catalogNotation); // カタログ表記の選択肢による修正
     const fuelCostDifference = (electricityCostPerKm - gasolineCostPerKm) * monthlyDistance;
 
     document.getElementById("gasolineCostPerKm").textContent = gasolineCostPerKm.toFixed(2);
